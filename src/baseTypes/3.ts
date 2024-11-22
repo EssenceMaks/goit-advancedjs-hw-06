5eb16894e7c1927a9266d1c2558072840f110512
Maxim_Kyianytsia <MaksKi.Wish@gmail.com>
2024-11-22 08:13:04 +0200

    Update 3.ts

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,18 @@
+/*
 let some:unknown;
 some = 'Text';
 let str: string;
 str = some;
+
+export {};
+*/
+
+let some: unknown;
+some = 'Text';
+let str: string;
+
+if (typeof some === 'string') {
+    str = some;
+}
 
 export {};
