5c7a8934ed24f1ecb2d79ae9b6c2e7509845a9cf
Maxim_Kyianytsia <MaksKi.Wish@gmail.com>
2024-11-22 08:15:28 +0200

    Update 8.ts

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -1,8 +1,14 @@
 /*
   Створіть тип "Gender", використовуючи union type, 
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
-*/
+
 
 const myGender;
+
+export {};
+*/
+
+type Gender = "male" | "female";
+const myGender: Gender = "male";
 
 export {};
