00adecd32fe3a2d3fd60cab5e3b6fd64312814d3
Maxim_Kyianytsia <MaksKi.Wish@gmail.com>
2024-11-22 08:19:57 +0200

    Update 6.ts

--- src/generic/6.ts
+++ src/generic/6.ts
@@ -5,7 +5,6 @@
   залишивши ім'я та прізвище без змін.
 
   Виправте тип у аргументі функції так, щоб не було помилок типу.
-*/
 
 type User = {
   name: string;
@@ -15,6 +14,22 @@ type User = {
 }
 
 function createOrUpdateUser(initialValues: User) {
+  // Оновлення користувача
+}
+
+createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
+
+export {};
+*/
+
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
+
+function createOrUpdateUser(initialValues: Partial<User>) {
   // Оновлення користувача
 }
 
