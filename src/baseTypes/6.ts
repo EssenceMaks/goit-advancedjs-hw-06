806087fc33f31ac970efbe7b4544595881cb3ac4
Maxim_Kyianytsia <MaksKi.Wish@gmail.com>
2024-11-22 08:14:37 +0200

    Update 6.ts

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -1,6 +1,6 @@
 /*
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
-*/
+
 
 function showMessage(message) {
   console.log(message);
@@ -11,6 +11,21 @@ function calc(num1, num2) {
 }
 
 function customError() {
+  throw new Error('Error');
+}
+
+export {};
+*/
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function customError(): never {
   throw new Error('Error');
 }
 
