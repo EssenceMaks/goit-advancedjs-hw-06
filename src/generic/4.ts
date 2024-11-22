0ca15549534aaa92f409bcc15a5096e84262170b
Maxim_Kyianytsia <MaksKi.Wish@gmail.com>
2024-11-22 08:18:10 +0200

    Update 4.ts

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,6 +1,6 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
-*/
+
 
 class Component {
   constructor (public props:T) {
@@ -10,6 +10,24 @@ class Component {
 
 class Page extends Component {
   pageInfo () {
+    console.log(this.props.title);
+  }
+}
+
+export {};
+*/
+
+interface Props {
+  title: string;
+}
+
+class Component<T> {
+  constructor(public props: T) {
+  }
+}
+
+class Page extends Component<Props> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
